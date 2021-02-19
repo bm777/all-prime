9d96636f04a7a1ee7daed2f89f230a5e3db18c1f
Bayangmbe Mounmo <bayangp0@gmail.com>
2021-02-19 22:15:30 +0100

    Added pi function

--- prime number/is_prime_number.py
+++ prime number/is_prime_number.py
@@ -12,20 +12,32 @@ import math
 import random
 
 def is_prime(number):
-    if number < 1:
-        pass
+    state = True
+    if number < 1 and number == 4:
+        return False
+    elif number == 2 or number == 3:
+        return True
     else:
-        for i in range(2, int(math.sqrt(number))):
+        for i in range(2, int(number/2)):
             if number % i == 0:
-                return False
+                state = False
                 break
-    return True
+        return state
 
+def pi(n):
+    result = []
+    for iter in range(n):
+        if(is_prime(iter) == True):
+            if iter % 100 == 0:
+                print(result)
+            result.append(iter)
 
+    return len(result)
 
 
 
 
 
 if __name__ == '__main__':
-    print(is_prime(1331))
+    print(pi(int(math.pow(10,14))))
+    # print(is_prime(9))
